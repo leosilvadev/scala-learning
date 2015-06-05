57492b174eaa157a341edff97ee53e636d8a4b0f
leosilvadev <leosilvadev@gmail.com>
2015-06-04 22:08:29 -0300

    Trying more about xml read

--- src/traits/collections/user/XmlReader.scala
+++ src/traits/collections/user/XmlReader.scala
@@ -2,10 +2,16 @@ package traits.collections.user
 
 object XmlReader extends App {
 
-  val xmlFile = xml.XML.loadFile("map.xml")
-  //You can go entering inside the xml tree by this :)
-  (xmlFile \ "room" \ "item" \ "@name") foreach { row =>
-    println(row)
-  }
+//  val xmlFile = xml.XML.loadFile("map.xml")
+//  //You can go entering inside the xml tree by this :)
+//  (xmlFile \ "room" \ "item" \ "@name") foreach { row =>
+//    println(row)
+//  }
   
+  val v = "method param1 param2"
+  val v2 = v.indexWhere(_.isWhitespace) match {
+    case -1 => (v, "")
+    case i => v.splitAt(i)
+  }
+  println(v2)
 }
